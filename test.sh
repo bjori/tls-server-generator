4d1ec1da4779812b297e1b96d67d053704ad2596
Shane Harvey <shane.harvey@mongodb.com>
2017-02-09 14:54:31 -0800

    Run tests with config files

--- test.sh
+++ test.sh
@@ -12,6 +12,14 @@ run_test() {
    printf "%10s: %s\n" "$result" "$uri"
 }
 
+run_config_test() {
+   config_file=$1
+   # Convert into base32 and split into 63 character labels.
+   encoded=$(base32 ${config_file} | tr -d '\n' | sed -E 's/(.{64})/\1./g')
+   encoded_host="${encoded}.vcap.me"
+   run_test "mongodb://${encoded_host}:8888/?ssl=true&sslCertificateAuthorityFile=$ROOT/tests/x509gen/ca.pem"
+}
+
 PASS="
 mongodb://im6uqskhja5ccrkykbhvevb2efqu4vkmjrafgvcsivheovcibjbucplsn5xxics.oii6tembrg4wteljrbjhecpjsgaytqljsfuyquu2bjy6usub2gezdolrqfyyc4m.ikifjquqsdhuzaus2vhuyqurklku6tocq.vcap.me:8888/?ssl=true&sslCertificateAuthorityFile=$ROOT/tests/x509gen/ca.pem
 ";
@@ -25,8 +33,15 @@ for i in $PASS; do
    run_test "$i"
 done
 
+for good_config in ./config/good*; do
+   run_config_test "$good_config"
+done
+
 echo "----------------------- SHOULD FAIL -----------------------"
 for i in $FAIL; do
    run_test "$i"
 done
 
+for bad_config in ./config/bad*; do
+   run_config_test "$bad_config"
+done
